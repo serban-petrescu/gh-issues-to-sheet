f529b4f293a16bfa8fd6fb0cc1256267885789cc
Serban Petrescu <Serban.Petrescu@outlook.com>
2023-01-03 16:38:16 +0200

    Fixes the action parameter handling.

--- action/src/index.ts
+++ action/src/index.ts
@@ -1,32 +1,31 @@
 import * as core from '@actions/core';
+import * as github from '@actions/github';
 import type { IssueType } from 'gh-issues-to-sheet-lib';
 import { toGoogleSheets } from 'gh-issues-to-sheet-lib';
 
 async function run(): Promise<void> {
     try {
         const exporter = toGoogleSheets({
-            githubToken: core.getInput('githubToken'),
-            googleCreds: JSON.parse(core.getInput('sheetCreds')),
+            githubToken: core.getInput('github-token', { required: true }),
+            googleCreds: JSON.parse(core.getInput('sheet-creds', { required: true })),
         });
         exporter.setLogger(core);
+        const { owner, repo } = github.context.repo;
         await exporter.exportIssues(
             {
-                repo: core.getInput('repository'),
-                types: core
-                    .getInput('issueTypes')
-                    .split(',')
-                    .map(s => s.trim().toLowerCase()) as IssueType[],
+                repo: core.getInput('repository') || `${owner}/${repo}`,
+                types: (core.getInput('issue-types') || 'issue').split(',').map(s => s.trim().toLowerCase()) as IssueType[],
             },
             {
-                sheetUrl: core.getInput('sheetUrl'),
-                deltaUpdate: core.getBooleanInput('deltaUpdate'),
+                sheetUrl: core.getInput('sheet-url', { required: true }),
+                deltaUpdate: core.getBooleanInput('delta-update') || false,
             }
         );
     } catch (error) {
         if (error instanceof Error) {
             core.setFailed(error.message);
         } else {
-            core.setFailed(`An unknown error occured: ${String(error)}`);
+            core.setFailed(`An unknown error occurred: ${String(error)}`);
         }
     }
 }
